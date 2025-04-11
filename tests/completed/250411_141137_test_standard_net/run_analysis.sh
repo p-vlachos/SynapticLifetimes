0d6f4fccda74b66068deb7de853442eaf97daa32
p-vlachos <pvlachos@fias.uni-frankfurt.de>
2025-04-11 16:10:20 +0200

    Removed simulation files that needed to be ignored

--- tests/completed/250411_141137_test_standard_net/run_analysis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf src/analysis
-cp -r ~/lab/netw-mod/analysis-dev/ src/analysis/
-
-#python -m code.analysis.overview
-#python -m src.analysis.t5_rasters
-python -m src.analysis.t5_peaks
